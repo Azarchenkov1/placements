48194007163cb1a5ad2b0b1487e70a97a66e0276
Nikita Azarchenkov <azarchenkov97@ukr.net>
2018-12-27 23:31:08 +0200

    imagetest_fix2

--- ClientApp/src/app/imagetest/imagetest.component.ts
+++ ClientApp/src/app/imagetest/imagetest.component.ts
@@ -1,4 +1,4 @@
-import { Component, Inject } from '@angular/core';
+import { Component } from '@angular/core';
 import { HttpClient, HttpRequest } from '@angular/common/http';
 
 @Component({
@@ -7,23 +7,23 @@ import { HttpClient, HttpRequest } from '@angular/common/http';
   styleUrls: ['./imagetest.component.css']
 })
 export class ImagetestComponent {
-  public str;
+  public imageset;
   constructor(private http: HttpClient) { }
   submitData(mainphoto)
   {
     const photoform = new FormData();
     photoform.append(mainphoto[0].name, mainphoto[0]);
     const photoFormSendRequest = new HttpRequest('POST', `api/home/returnimagelist` , photoform);
-this.http.request(photoFormSendRequest)
-.subscribe(response => {
-console.log("photos was submited<---------------||");
-var response_data = response;
-if(response_data != null)
-{
-         console.log('string successfuly received');
-         console.log(response_data);
-         this.str = response_data;
-}
-});
+    this.http.request(photoFormSendRequest)
+    .subscribe(response => 
+    {
+      console.log("photos was submited<---------------||");
+      this.imageset = response;
+      if(this.imageset != null)
+      {
+        console.log('string successfuly received');
+        console.log(this.imageset);
+      }
+    });
   }
 }
